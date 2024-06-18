0169f9e69f3098c99971cc7bdd987f7cb8e17a7a
Davy Yao <ym521105@gmail.com>
2024-06-18 17:51:26 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const {version} = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -13,4 +14,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps()
+    .version();
