60a2f0143e34b968e67c1c7ba7f4f02e4c913d98
Davy Yao <ym521105@gmail.com>
2024-07-05 18:29:17 +0800

    查看用户列表

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //创建50个并为第一条数据更新信息
+        User::factory()->count(50)->create();
+        $user = User::find(1);
+        $user->name = 'DavvYao';
+        $user->email = 'ym521105@gmail.com';
+        $user->save();
+    }
+}
