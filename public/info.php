2cf03f20a2545676bd114531fd2ac681827d54c2
Davy Yao <ym521105@gmail.com>
2024-07-19 10:55:00 +0800

    访问限流，移除邮件发送的 from 选项

--- public/info.php
+++ public/info.php
@@ -1,5 +1,7 @@
 <?php
-echo date('Y-m-d H:i:s','1721198093').'<br/>';
-echo date('Y-m-d H:i:s','1721200742').'<br/>';
-echo strtotime('2024-07-16 00:00:00').'<br/>';
+echo date('Y-m-d H:i:s','1705820174').'<br/>';
+echo date('Y-m-d H:i:s','1721198020').'<br/>';
+echo strtotime('2024-07-17 15:52:27').'<br/>';
+echo strtotime('2024-07-17 16:34:26').'<br/>';
+echo strtotime('2024-07-18 00:00:00').'<br/>';
 //phpinfo();
