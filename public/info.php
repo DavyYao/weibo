e14f18442de35b103ee3012220b10952dc0532d0
Davy Yao <ym521105@gmail.com>
2024-07-25 19:10:02 +0800

    删除微博

--- public/info.php
+++ public/info.php
@@ -5,3 +5,5 @@ echo strtotime('2024-07-17 15:52:27').'<br/>';
 echo strtotime('2024-07-17 16:34:26').'<br/>';
 echo strtotime('2024-07-18 00:00:00').'<br/>';
 //phpinfo();
+$arr = [2,'','',3,'','',4,'','',5,'','',16,'','',11,];
+var_dump(in_array(1,$arr));
