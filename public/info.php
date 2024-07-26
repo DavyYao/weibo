b45b8a860a61b19264883914a00f3f85dea37d35
Davy Yao <ym521105@gmail.com>
2024-07-26 19:13:32 +0800

    粉丝数据表

--- public/info.php
+++ public/info.php
@@ -1,6 +1,6 @@
 <?php
-echo date('Y-m-d H:i:s','1705820174').'<br/>';
-echo date('Y-m-d H:i:s','1721198020').'<br/>';
+echo date('Y-m-d H:i:s','1721577600').'<br/>';
+echo date('Y-m-d H:i:s','1721467361').'<br/>';
 echo strtotime('2024-07-17 15:52:27').'<br/>';
 echo strtotime('2024-07-17 16:34:26').'<br/>';
 echo strtotime('2024-07-18 00:00:00').'<br/>';
